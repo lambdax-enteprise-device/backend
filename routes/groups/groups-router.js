056b94afd77de38a453c68d95efb635ec816e83f
Joel Perez <joelness03@gmail.com>
2020-01-10 11:01:37 -0700

    created groups routes

---
+++ routes/groups/groups-router.js
@@ -0,0 +1,53 @@
+const router = require("express").Router();
+
+const Groups = require("../../data-models/groups/groups-model.js");
+
+// Get all Manufacturers
+router.get("/", (req, res) => {
+  Groups.findAllGroups()
+    .then(groups => {
+      res.status(200).json(groups);
+    })
+    .catch(error => {
+      res.status(500).json({ message: "Could not get groups", error: error });
+    });
+});
+
+router.post("/", (req, res) => {
+  const group = req.body;
+  Groups.add(group)
+    .then(group => {
+      res.status(200).json(group);
+    })
+    .catch(error => {
+      res.status(500).json({ message: "Unable to add group", error: error });
+    });
+});
+
+router.get("/:id", (req, res) => {
+  const { id } = req.params;
+
+  Groups.findById(id)
+    .then(group => {
+      res.status(200).json(group);
+    })
+    .catch(error => {
+      res
+        .status(500)
+        .json({ message: "Unable to retrieve group", error: error });
+    });
+});
+
+router.delete("/:id", (req, res) => {
+  const { id } = req.params;
+
+  Groups.remove(id)
+    .then(deletedGroup => {
+      res.status(204).end();
+    })
+    .catch(error => {
+      res.status(500).json({ message: "Group with that ID not found" });
+    });
+});
+
+module.exports = router;
