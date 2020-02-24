ea84039c034dd7739f97d364e87a0f02f8449543
Joel Perez <joelness03@gmail.com>
2020-02-24 14:09:45 -0700

    signup endpoint and model complete

--- data-models/auth/auth-model.js
+++ data-models/auth/auth-model.js
@@ -1,35 +1,57 @@
 const db = require("../../data/db-config.js");
 
+const generateToken = require("../../utils/auth/generateToken");
+
 module.exports = {
-  signUp
+  signUp,
+  find
 };
 
 async function signUp(company, user) {
+  company.active = true;
   const [companyId] = await db("companies").insert(company, "id");
   user["company_id"] = companyId;
 
   const [userId] = await db("users").insert(user, "id");
 
   //* Creation of Administrators group and role, assign user to group and group to role
-  const [groupdId] = await db("groups").insert({
-    company_id: companyId,
-    group_name: "Administrators"
-  });
-  const [roleId] = await db("roles").insert({
-    company_id: companyId,
-    name: "Administrators",
-    description:
-      "Role used for Administrators of company. Full permissions given",
-    assignment: "Company-wide",
-    access_level: 1
-  });
-  await db("role_groups").insert({
-    group_id: groupdId,
-    role_id: roleId
-  });
-  await db("users_groups").insert({
-    user_id: userId,
-    group_id: groupdId
-  });
-  return findById(companyId);
+  const [groupId] = await db("groups").insert(
+    {
+      company_id: companyId,
+      group_name: "Administrators"
+    },
+    "id"
+  );
+  const [roleId] = await db("roles").insert(
+    {
+      company_id: companyId,
+      name: "Administrators",
+      description:
+        "Role used for Administrators of company. Full permissions given",
+      assignment: "Company",
+      access_level: 5
+    },
+    "id"
+  );
+  const [roleGroupId] = await db("role_groups").insert(
+    {
+      group_id: groupId,
+      role_id: roleId
+    },
+    "id"
+  );
+  const [usersGroupsId] = await db("users_groups").insert(
+    {
+      user_id: userId,
+      group_id: groupId
+    },
+    "id"
+  );
+
+  user.company_name = company.company_name;
+  return user;
+}
+
+async function find(filter) {
+  return db("users").where(filter);
 }
