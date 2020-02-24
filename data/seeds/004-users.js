f426796bf189cfe96f8ce7f70f86634d03144590
Joel Perez <joelness03@gmail.com>
2020-02-24 10:57:17 -0700

    updated seed to include password

--- data/seeds/004-users.js
+++ data/seeds/004-users.js
@@ -8,6 +8,8 @@ exports.seed = function(knex) {
         {
           company_id: 2,
           email: "joel.perez@testcompany.com",
+          password:
+            "$2b$10$01efxyrpX0iFApoZvyYs5OVNtFTdAQXon1fvUQMhP1wp8w2oyOEIG",
           first_name: "Joel",
           last_name: "Perez",
           title: "CTO"
