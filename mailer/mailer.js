727847764f12706e37bc4bf955659b58070da795
Mike Harley <info@mike-harley.tech>
2020-05-05 12:25:11 -0700

    Update mailer.js

--- mailer/mailer.js
+++ mailer/mailer.js
@@ -1,7 +1,7 @@
 require('dotenv').config()
 const mailgun = require('mailgun-js')
 const DOMAIN = "sandbox8a329a5d78ff4cbf94fc98e636de2930.mailgun.org"
-const api_key = "c953845a3989623b17af555c815e5aa6-0afbfc6c-e25c02dd"
+const api_key = ""
 const mg = mailgun({apiKey:api_key,domain:DOMAIN});
 const data = {
     from:"Node Test <test@sandbox8a329a5d78ff4cbf94fc98e636de2930.mailgun.org>",
